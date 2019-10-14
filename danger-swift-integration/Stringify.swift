cd5da481efd11dd30b4fe520f090556f2cab2f14
Akshay Kolte <akolte@athenahealth.com>
2019-10-15 02:13:36 +0530

    Setting up danger

---
+++ danger-swift-integration/Stringify.swift
@@ -0,0 +1,9 @@
+//
+//  Stringify.swift
+//  danger-swift-integration
+//
+//  Created by Akshay Kolte on 10/15/19.
+//  Copyright © 2019 akshaykolte. All rights reserved.
+//
+
+import Foundation
---
+++ danger-swift-integration/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  danger-swift-integration
+//
+//  Created by Akshay Kolte on 10/15/19.
+//  Copyright © 2019 akshaykolte. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
